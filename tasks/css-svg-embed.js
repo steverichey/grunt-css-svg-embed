bce7ff89c0e55b52dbf0c1bd2dd90ab5b84b3526
steverichey <stevenpatrickrichey@gmail.com>
2014-10-19 16:34:36 -0500

    initial setup

---
+++ tasks/css-svg-embed.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+    function someFunction() {
+        // nothing yet!
+    }
+    
+    grunt.registerMultiTask('csssvgembed', "Convert SVGs linked as URL's in your CSS to strings inside your style sheet", function() {
+        var async = this.async();
+        
+        var options = this.options({
+            failOnMissingUrl: true
+        });
+        
+        var existingFiles = this.files.filter(function(file) {
+            if (!grunt.file.exists(file.src[0])) {
+                return false;
+            }
+            
+            return true;
+        });
+        
+        var leftToProcess = existingFiles.length;
+        
+        existingFiles.forEach(function(file) {
+            processFile(file.src[0], file.dest, options, function() {
+                if (--leftToProcess === 0) {
+                    async();
+                }
+            });
+        });
+    });
+};
